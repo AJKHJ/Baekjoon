0f91f71168b5c3334750ba737a36ffb3ac6b48ad
KOHUIJUN <gmlwns904@ajou.ac.kr>
2024-11-21 19:48:18 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,45 +1,31 @@
 #include <bits/stdc++.h>
-
-using namespace std;
-typedef long long ll;
-int h, w;
-char arr[104][104];
-int arr2[104][104];
-char c;
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(NULL);
-    cin >> h >> w;
-    
-    for(int i = 0 ; i < h ; i++){
-        for(int j = 0 ; j < w ; j++){
-            cin >> c;
-            arr[i][j] = c;
-            if(c == 'c'){
-                arr2[i][j] = 0;
-            }
-            else{
-                arr2[i][j] = -1;
-            }
-        }
-    }
-    for(int i = 0 ; i < h ; i++){
-        int lastCloud = -1;
-        for(int j = 0 ; j < w ; j++){
-            if (arr[i][j] == 'c') {
-                lastCloud = j;
-                arr2[i][j] = 0;
-            } else if (lastCloud != -1) {
-                arr2[i][j] = j - lastCloud;
-            }
-        }
-    }
-    for(int i = 0 ; i < h ; i++){
-        for(int j = 0 ; j < w ; j++){
-            cout << arr2[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
+using namespace std;  
+int n, m, a[104][104];
+string s; 
+int main () {
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++){
+		cin >> s; 
+		for(int j = 0; j < m; j++){ 
+			if(s[j] == '.')a[i][j] = -1;
+			else a[i][j] = 0;
+		}
+	}
+	for(int i = 0; i < n; i++){ 
+		for(int j = 0; j < m; j++){
+			if(a[i][j] == 0){ 
+				int cnt = 0;
+				while(a[i][j + 1] == -1){
+                    cnt++;
+					a[i][j + 1] = cnt;
+					j++;
+				} 
+			}
+		} 
+	} 
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++) cout << a[i][j] << " ";
+		cout << "\n";
+	} 
+	return 0;
 }
